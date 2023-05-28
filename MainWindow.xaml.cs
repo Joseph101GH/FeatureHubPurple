28835b2e9c96fdb5b6fae41af54e7e180e4b10d3
Joseph101GH <joseph1@live.be>
2023-05-28 18:59:55 +0200

    Working app. Feature 1 done.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using FeatureHubPurple.UserControls;
 
 namespace FeatureHubPurple
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow Instance
+        {
+            get; private set;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            Instance = this;
+        }
+ 
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = this.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Feature1Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetTitleContent();
+            LoadFeature1();
+        }
+
+        private void Feature2Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoadFeature2();
+        }
+
+        private void Feature3Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoadFeature3();
+        }
+
+        private void LoadFeature1()
+        {
+            var content = new Feature1Control();
+            MainContent.Children.Clear();
+            MainContent.Children.Add(content);
+        }
+
+        private void SetTitleContent()
+        {
+            pageTitle.Text = "GUID-Checker";
+        }
+
+        private void LoadFeature2()
+        {
+            var content = new Feature2Control();
+            MainContent.Children.Clear();
+            MainContent.Children.Add(content);
+        }
+
+        private void LoadFeature3()
+        {
+            var content = new Feature3Control();
+            MainContent.Children.Clear();
+            MainContent.Children.Add(content);
+        }
+
+        private void Feature4Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Feature5Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HistoryButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
--- UserControls/Feature1Control.xaml.cs
+++ UserControls/Feature1Control.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Converters;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -24,5 +26,49 @@ namespace FeatureHubPurple.UserControls
         {
             InitializeComponent();
         }
+
+        private void InputTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (IsGuid(InputTextBox.Text))
+            {
+                //CORRECT
+              MainWindow.Instance.mainContentBorder.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#059547"));
+            }
+            else
+            {
+                //WRONG
+                MainWindow.Instance.mainContentBorder.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#e0443c"));
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBox.Clear();
+            this.Background = Brushes.Transparent;
+        }
+
+        private void CopyClipboard_Click(object sender, RoutedEventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                InputTextBox.Text = Clipboard.GetText();
+            }
+        }
+
+        private void guidExample_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBox.Text = "6B29FC40-CA47-1067-B31D-00DD010662DA";
+
+        }
+
+        private bool IsGuid(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+            Regex guidPattern = new Regex(@"^(\{0[xX][0-9a-fA-F]{8}\,(\s)?[xX][0-9a-fA-F]{4}\,(\s)?[xX][0-9a-fA-F]{4}\,(\s)?\{[0-9a-fA-F]{2}(\s)?,[0-9a-fA-F]{2}(\s)?,[0-9a-fA-F]{2}(\s)?,[0-9a-fA-F]{2}(\s)?,[0-9a-fA-F]{2}(\s)?,[0-9a-fA-F]{2}(\s)?,[0-9a-fA-F]{2}(\s)?,[0-9a-fA-F]{2}(\s)?\}\})$|^(?i)[0-9a-fA-F]{8}[-][0-9a-fA-F]{4}[-][0-9a-fA-F]{4}[-][0-9a-fA-F]{4}[-][0-9a-fA-F]{12}$");
+            return guidPattern.IsMatch(input);
+        }
+
+
     }
 }
