42889e5f7172f029813d54f88751a09106891949
Joseph101GH <joseph1@live.be>
2023-05-28 17:15:53 +0200

    Add project files.

---
+++ UserControls/Feature3Control.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FeatureHubPurple.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Feature3Control.xaml
+    /// </summary>
+    public partial class Feature3Control : UserControl
+    {
+        public Feature3Control()
+        {
+            InitializeComponent();
+        }
+    }
+}
